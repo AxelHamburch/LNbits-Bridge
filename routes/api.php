ed048e86853b3d559f5fddb51f2f01b60b4523c3
axelhamburch <axelhamburch@protonmail.com>
2023-10-18 21:17:58 +0200

    api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route heißt /api/counter
+Route::get('/counter', function () {
+    return view('welcome');
+});
+
+// Route heißt /api/balance
+Route::get('/balance', function () {
+    return view('welcome');
+});
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/webhook/onboarding', function (Request $request) {
+   dump($request->all());
+});
+
+
